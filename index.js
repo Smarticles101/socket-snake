8c2625d79f0c5ac6d8f8c3701f10d9d5fbe98b9f
Logan Stucki <loganstucki@gmail.com>
2021-10-31 16:54:41 -0600

    implement initial multiplayer functions

--- index.js
+++ index.js
@@ -1,23 +1,46 @@
-const { application } = require('express');
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http');
+const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.get('/snake.js', (req, res) => {
-  res.sendFile(__dirname + '/snake.js');
+app.get("/snake.js", (req, res) => {
+  res.sendFile(__dirname + "/snake.js");
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+app.get("/socket-snake.js", (req, res) => {
+  res.sendFile(__dirname + "/socket-snake.js");
+});
+
+let players = {};
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  player = {
+    id: socket.id,
+    x: Math.floor(Math.random() * 50) * 10,
+    y: Math.floor(Math.random() * 50) * 10,
+  };
+
+  socket.emit("players", players);
+  socket.emit("init", player);
+  socket.broadcast.emit("init", player);
+
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    console.log("a user disconnected");
+    socket.broadcast.emit("disconnected", socket.id);
+  });
+
+  players[socket.id] = player;
 });
 
 server.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
